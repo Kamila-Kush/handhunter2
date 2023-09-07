0f78535d411221ff895f5fb8927657618f62fcc4
Kamila-Kush <kush.kamila@gmail.com>
2023-09-07 18:04:14 +0900

    update base.html, plug in static files

--- hh2/urls.py
+++ hh2/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from core.views import *
 
@@ -25,4 +27,4 @@ urlpatterns = [
     path('about_us/', about_us, name='about_us'),
 
     path('workers/', include('worker.urls'))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
